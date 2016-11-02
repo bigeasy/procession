ff89588d6f268ec74c6196724d36b9afc6e440d3
Alan Gutierrez <alan@prettyrobots.com>
2016-11-01 20:12:16 -0500

    Use `setImmediate`.

--- consumer.js
+++ consumer.js
@@ -31,7 +31,7 @@ Consumer.prototype._nudge = function () {
     if (this._callback != null) {
         var callback = [ this._callback, this._callback = null ][0]
         this._head = this._head.next
-        callback(null, this._head.value)
+        setImmediate(callback, null, this._head.value)
     }
 }
 
