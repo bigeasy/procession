761e4719fbad1588ac0364cf82c6f10bece9e69a
Alan Gutierrez <alan@prettyrobots.com>
2017-03-13 11:24:26 -0500

    Sketch of a transformer.

---
+++ t/transformer.t.js
@@ -0,0 +1,20 @@
+require('proof/redux')(1, require('cadence')(prove))
+
+function prove (async, assert) {
+    var Procession = require('..')
+    var Transformer = require('../transformer')
+    var first = new Procession
+    var second = new Procession
+    var third = new Procession
+    first.name = 'first'
+    second.name = 'second'
+    first.pump(new Transformer(function (value, callback) {
+        callback(null, value + 1)
+    }, second))
+    second.pump(new Transformer(function (value) {
+        return value + 1
+    }, third))
+    var shifter = third.shifter()
+    first.push(1)
+    assert(shifter.shift(), 3, 'transformed')
+}
---
+++ transformer.js
@@ -0,0 +1,36 @@
+// Sketch of a tranform. No use for it yet.
+//
+// Maybe it is also a filter where if you return `null` from the transform
+// operation the value is not enqueued.
+
+// First use of Operation variadic helpers.
+
+//
+var Operation = require('operation/variadic')
+var cadence = require('cadence')
+
+function Transformer () {
+    var vargs = Array.prototype.slice.call(arguments)
+    var operation = Operation(vargs)
+    if (operation.length == 2) {
+        this._operation = operation
+    } else {
+        this._operation = function (value, callback) {
+            callback(null, operation(value))
+        }
+    }
+    this._write = vargs.shift()
+}
+
+Transformer.prototype.enqueue = cadence(function (async, value, callback) {
+    async(function () {
+        if (value == null) {
+            return [ value ]
+        }
+        this._operation(value, async())
+    }, function (value) {
+        this._write.enqueue(value, async())
+    })
+})
+
+module.exports = Transformer
